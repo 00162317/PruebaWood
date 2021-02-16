fefae73fcb724de1b9715e61b82fed2d1359a842
David Guardado <00184217@uca.edu.sv>
2021-02-16 16:12:20 -0600

    raiz heroku

--- ejemploLabo5/src/main/java/com/Prueba/Wood/controller/MainController.java
+++ ejemploLabo5/src/main/java/com/Prueba/Wood/controller/MainController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -104,7 +105,10 @@ public class MainController {
 
 		return "redirect:/status";
 	}
-	
+	@GetMapping("/status")
+	public String status() {
+		return "status";
+	}
 	
 	
 }
