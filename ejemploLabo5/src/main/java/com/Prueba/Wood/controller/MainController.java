71e8bd3aa0d9d56bd84fce9391f0161f3662b830
David Guardado <00184217@uca.edu.sv>
2021-02-16 14:28:49 -0600

    se suben a mi pc, como localhost y se guarda el url

--- ejemploLabo5/src/main/java/com/Prueba/Wood/controller/MainController.java
+++ ejemploLabo5/src/main/java/com/Prueba/Wood/controller/MainController.java
@@ -1,13 +1,22 @@
 package com.Prueba.Wood.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.Prueba.Wood.dao.UsuarioDAO;
 import com.Prueba.Wood.domain.Usuario;
@@ -58,12 +67,44 @@ public class MainController {
 		return mav;
 	}
 	
-	@RequestMapping("/ingresar")
+/*	@RequestMapping("/ingresar")
 	public ModelAndView ingresar(@ModelAttribute Usuario usuario) {
 		ModelAndView mav = new ModelAndView();
 		usuarioDAO.insert(usuario);
 		mav.addObject("usuario", usuario);
 		mav.setViewName("respuesta2");
 		return mav;
+	}*/
+	
+	
+	@PostMapping("/ingresar")
+	public String uploadFile(@RequestParam("file") MultipartFile file, @ModelAttribute Usuario usuario, RedirectAttributes attributes)
+			throws IOException {
+
+		if (file == null || file.isEmpty()) {
+			attributes.addFlashAttribute("message", "Por favor seleccione un archivo");
+			return "redirect:/index";
+		}
+
+		StringBuilder builder = new StringBuilder();
+		builder.append(System.getProperty("user.home"));
+		builder.append(File.separator);
+		builder.append("git\\demo-spring-fileupload\\demo-spring-fileupload\\src\\main\\resources\\templates");
+		builder.append(File.separator);
+		builder.append(file.getOriginalFilename());
+
+		byte[] fileBytes = file.getBytes();
+		Path path = Paths.get(builder.toString());
+		Files.write(path, fileBytes);
+		usuario.setImg(file.getOriginalFilename().toString());
+		usuarioDAO.insert(usuario);
+		
+		
+		attributes.addFlashAttribute("message", "Archivo cargado correctamente ["+builder.toString()+"]");
+
+		return "redirect:/index";
 	}
+	
+	
+	
 }
--- ejemploLabo5/src/main/java/com/Prueba/Wood/domain/Usuario.java
+++ ejemploLabo5/src/main/java/com/Prueba/Wood/domain/Usuario.java
@@ -8,43 +8,57 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(schema="public",name="usuario")
+@Table(schema = "public", name = "usuario")
 public class Usuario {
 
 	@Id
-	@Column(name="id_usuario")
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "id_usuario")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id_usuario;
-	
-	@Column(name="nombre")
+
+	@Column(name = "nombre")
 	private String nombre;
-	
-	@Column(name="apellido")
+
+	@Column(name = "apellido")
 	private String apellido;
-	
+
+	@Column(name = "img")
+	private String img;
+
 	public Usuario() {
-		
+
 	}
-	
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
 	public Integer getId_usuario() {
 		return id_usuario;
 	}
+
 	public void setId_usuario(Integer id_usuario) {
 		this.id_usuario = id_usuario;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getApellido() {
 		return apellido;
 	}
+
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
-	
-	
-	
+
 }
