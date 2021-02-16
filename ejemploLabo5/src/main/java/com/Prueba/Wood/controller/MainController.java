9f6383c3ff95a9ac61927a4a0f616a73c9a925f3
David Guardado <00184217@uca.edu.sv>
2021-02-16 15:24:22 -0600

    a

--- ejemploLabo5/src/main/java/com/Prueba/Wood/controller/MainController.java
+++ ejemploLabo5/src/main/java/com/Prueba/Wood/controller/MainController.java
@@ -87,7 +87,7 @@ public class MainController {
 		}
 
 		StringBuilder builder = new StringBuilder();
-		builder.append("/00162317/PruebaWood/tree/master/ejemploLabo5");
+		builder.append(System.getProperty("user.home"));
 		builder.append(File.separator);
 		builder.append(file.getOriginalFilename());
 
